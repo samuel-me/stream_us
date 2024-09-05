8fa6626f4f24bdffc6cd647ebc633993fb7ee38c
samuel-me <72659273+samuel-me@users.noreply.github.com>
2024-09-05 03:50:33 +0100

    Add files via upload

--- me.py
+++ me.py
@@ -15,32 +15,6 @@ contact_form = """
 
 st.markdown(contact_form, unsafe_allow_html=True)
 
-import streamlit as st
-import os
-
-def download_pdf(pdf_path):
-    """Downloads a PDF from the specified path."""
-    if not os.path.exists(pdf_path):
-        st.error(f"PDF file '{pdf_path}' not found.")
-        return
-
-    with open(pdf_path, 'rb') as f:
-        pdf_bytes = f.read()
-
-    st.download_button(
-        label="Download PDF",
-        data=pdf_bytes,
-        file_name=os.path.basename(pdf_path),
-        mime="application/pdf"
-    )
-
-# Replace 'path/to/your/pdf.pdf' with the actual path to your PDF file
-pdf_path = "h.pdf"
-
-# Create a Streamlit button to trigger the download
-if st.button("fonwload??? "):
-    download_pdf(pdf_path)
-
 
 #st.download_button('DOwnload button',data = 'h.csv', file_name = 'Summary.csv', mime ="text/docx")
 # Use Local CSS File
